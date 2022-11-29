d08e6459c3467b609640b1bc0ed8bae86f7cade0
Christian Hoffmeister <mail@choffmeister.de>
2022-11-29 12:54:39 +0100

    improve tests

--- internal/registry_docker_test.go
+++ internal/registry_docker_test.go
@@ -32,44 +32,32 @@ func TestDockerRetrieveLabels(t *testing.T) {
 	t.Run("docker-v2-manifest", func(t *testing.T) {
 		output, err := reg.RetrieveLabels("airfocusio/git-ops-update-test", "docker-v2-manifest-v0.0.1")
 		assert.NoError(t, err)
-		assert.Equal(t, map[string]string{
-			"io.buildah.version":                "1.23.1",
-			"org.opencontainers.image.version":  "0.0.1",
-			"org.opencontainers.image.source":   "https://github.com/airfocusio/git-ops-update",
-			"org.opencontainers.image.revision": "99a7aaa2eff5aff7c77d9caf0901454fedf6bf00",
-		}, output)
+		assert.Equal(t, "0.0.1", output["org.opencontainers.image.version"])
+		assert.Equal(t, "https://github.com/airfocusio/git-ops-update", output["org.opencontainers.image.source"])
+		assert.Equal(t, "99a7aaa2eff5aff7c77d9caf0901454fedf6bf00", output["org.opencontainers.image.revision"])
 	})
 
 	t.Run("oci-v1-manifest", func(t *testing.T) {
 		output, err := reg.RetrieveLabels("airfocusio/git-ops-update-test", "oci-v1-manifest-v0.0.1")
 		assert.NoError(t, err)
-		assert.Equal(t, map[string]string{
-			"io.buildah.version":                "1.23.1",
-			"org.opencontainers.image.version":  "0.0.1",
-			"org.opencontainers.image.source":   "https://github.com/airfocusio/git-ops-update",
-			"org.opencontainers.image.revision": "99a7aaa2eff5aff7c77d9caf0901454fedf6bf00",
-		}, output)
+		assert.Equal(t, "0.0.1", output["org.opencontainers.image.version"])
+		assert.Equal(t, "https://github.com/airfocusio/git-ops-update", output["org.opencontainers.image.source"])
+		assert.Equal(t, "99a7aaa2eff5aff7c77d9caf0901454fedf6bf00", output["org.opencontainers.image.revision"])
 	})
 
 	t.Run("docker-v2-manifest-list", func(t *testing.T) {
 		output, err := reg.RetrieveLabels("airfocusio/git-ops-update-test", "docker-v2-manifest-list-v0.0.1")
 		assert.NoError(t, err)
-		assert.Equal(t, map[string]string{
-			"io.buildah.version":                "1.23.1",
-			"org.opencontainers.image.version":  "0.0.1",
-			"org.opencontainers.image.source":   "https://github.com/airfocusio/git-ops-update",
-			"org.opencontainers.image.revision": "99a7aaa2eff5aff7c77d9caf0901454fedf6bf00",
-		}, output)
+		assert.Equal(t, "0.0.1", output["org.opencontainers.image.version"])
+		assert.Equal(t, "https://github.com/airfocusio/git-ops-update", output["org.opencontainers.image.source"])
+		assert.Equal(t, "99a7aaa2eff5aff7c77d9caf0901454fedf6bf00", output["org.opencontainers.image.revision"])
 	})
 
 	t.Run("oci-v1-image-index", func(t *testing.T) {
 		output, err := reg.RetrieveLabels("airfocusio/git-ops-update-test", "oci-v1-image-index-v0.0.1")
 		assert.NoError(t, err)
-		assert.Equal(t, map[string]string{
-			"io.buildah.version":                "1.23.1",
-			"org.opencontainers.image.version":  "0.0.1",
-			"org.opencontainers.image.source":   "https://github.com/airfocusio/git-ops-update",
-			"org.opencontainers.image.revision": "99a7aaa2eff5aff7c77d9caf0901454fedf6bf00",
-		}, output)
+		assert.Equal(t, "0.0.1", output["org.opencontainers.image.version"])
+		assert.Equal(t, "https://github.com/airfocusio/git-ops-update", output["org.opencontainers.image.source"])
+		assert.Equal(t, "99a7aaa2eff5aff7c77d9caf0901454fedf6bf00", output["org.opencontainers.image.revision"])
 	})
 }
